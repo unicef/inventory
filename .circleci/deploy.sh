678e74ac861ff30ac7b4948aff228d1e8e00f043
Justin W. Flory (he/him) [UNICEF Innovation] <jflory@unicef.org>
2021-01-15 18:55:05 -0500

    :beetle: :wrench: ci: Bring Circle CI config files into build path
    This is a workaround to make sure the `deploy.sh` script can be found
    when it is called in the `deploy` step.
    
    Signed-off-by: Justin W. Flory (he/him) [UNICEF Innovation] <jflory@unicef.org>

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -5,22 +5,23 @@
 
 set -e
 
-DEPLOY_DIR=deploy
+DEPLOY_DIR=~/project
+
+# trust GitHub server keys
+mkdir ~/.ssh/
+ssh-keyscan github.com >> ~/.ssh/known_hosts
+
+# stage generated HTML for GitHub Pages
+git clone --quiet --branch=gh-pages $CIRCLE_REPOSITORY_URL $DEPLOY_DIR
+rsync --archive --recursive --verbose --remove-source-files $HOME/hugo/inventory/public/* $DEPLOY_DIR
 
 # git client setup
+cd $DEPLOY_DIR
 git config --global push.default simple
 git config --global user.email $(git --no-pager show --no-patch --format='%ae' HEAD)
 git config --global user.name $CIRCLE_USERNAME
 
-# clone git repo with generated HTML
-git clone --quiet --branch=gh-pages $CIRCLE_REPOSITORY_URL $DEPLOY_DIR
-
-# keep only the built HMTL
-cd $DEPLOY_DIR
-rsync --archive --recursive --verbose --remove-source-files $HUGO_BUILD_DIR .
-rmdir public/
-
 # force push to GitHub Pages
 git add --force .
-git commit --msg="Deploy build $CIRCLE_BUILD_NUM [ci skip]" || true
+git commit --message="Deploy build $CIRCLE_BUILD_NUM [ci skip]" || true
 git push --force origin gh-pages
