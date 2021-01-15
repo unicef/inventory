c2739199862b5a703da9d41896f05f34ee33c0a8
J.W.F. (he/him) <4721034+jwflory@users.noreply.github.com>
2021-01-15 17:26:50 -0500

    :beetle: :wrench: ci: Use apt, not apk, in deploy step (#2)
    The CI environment deploys from Ubuntu 18.04. I was using the Alpine
    package manager, `apk`, but of course it is not installed. This should
    fix this issue.
    
    I have to merge this change to `main` to evaluate whether it will deploy
    the site or not.
    
    Signed-off-by: Justin W. Flory (he/him) [UNICEF Innovation] <jflory@unicef.org>

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -7,15 +7,20 @@ set -e
 
 DEPLOY_DIR=deploy
 
+# git client setup
 git config --global push.default simple
 git config --global user.email $(git --no-pager show --no-patch --format='%ae' HEAD)
 git config --global user.name $CIRCLE_USERNAME
 
+# clone git repo with generated HTML
 git clone --quiet --branch=gh-pages $CIRCLE_REPOSITORY_URL $DEPLOY_DIR
 
+# keep only the built HMTL
 cd $DEPLOY_DIR
-rsync --archive --recursive --verbose --delete ../public/* .
+rsync --archive --recursive --verbose --remove-source-files $HUGO_BUILD_DIR .
+rmdir public/
 
+# force push to GitHub Pages
 git add --force .
 git commit --msg="Deploy build $CIRCLE_BUILD_NUM [ci skip]" || true
 git push --force origin gh-pages
