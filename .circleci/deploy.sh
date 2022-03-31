d25937745a72f301d68e61cd5985afbf037b0e3e
Justin W. Flory (he/him) [UNICEF Innovation] <jflory@unicef.org>
2022-03-31 11:26:53 -0400

    bug: :wrench: ci: Fix GH Pages push with string comparison operator
    This commit changes the string comparison operator for the git user name
    in the deploy script used in continuous integration. Currently the check
    is checking if the string is not empty, then it overrides what was set.
    We actually wanted the inverse.
    
    Signed-off-by: Justin W. Flory (he/him) [UNICEF Innovation] <jflory@unicef.org>

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -21,7 +21,7 @@ rsync --archive --recursive --verbose --remove-source-files $HOME/hugo/$CIRCLE_P
 cd "$DEPLOY_DIR"
 git config --global push.default simple
 git config --global user.email "$(git --no-pager show --no-patch --format='%ae' HEAD)"
-if [ -n $CIRCLE_USERNAME ]; then
+if [ -z $CIRCLE_USERNAME ]; then
   git config --global user.name "$CIRCLE_USERNAME"
 else
   git config --global user.name "Committer not registered on CircleCI"
